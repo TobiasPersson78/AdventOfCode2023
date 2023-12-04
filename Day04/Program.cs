c16a74f356b452e19a33d96af821cddf156be9d2
Tobias Persson <tobias@tobiaspersson.se>
2023-12-04 07:51:07 +0100

    Day 4 solution

---
+++ Day04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+bool useExampleInput = false;
+string inputFilename = useExampleInput
+	? "exampleInput.txt"
+	: "input.txt";
+
+IList<int> matchesPerCard = File
+	.ReadAllLines(inputFilename)
+	.Select(line => line.Split(':', '|'))
+	.Select(indexWinnersAndActualParts =>
+		Regex
+			.Matches(indexWinnersAndActualParts[1], @"\d+")
+			.Select(item => item.Value)
+			.Intersect(
+				Regex
+					.Matches(indexWinnersAndActualParts[2], @"\d+")
+					.Select(item => item.Value))
+			.Count())
+	.ToList();
+int sumOfScratchCardPoints = matchesPerCard
+	.Where(item => item != 0)
+	.Sum(item => 1 << (item - 1));
+
+IList<int> numberOfCards = Enumerable.Repeat(1, matchesPerCard.Count).ToList();
+for (int currentCardIndex = 0; currentCardIndex < matchesPerCard.Count; ++currentCardIndex)
+{
+	for (int wonCardIndex = currentCardIndex + 1;
+		wonCardIndex < currentCardIndex + 1 + matchesPerCard[currentCardIndex];
+		++wonCardIndex)
+	{
+		numberOfCards[wonCardIndex] += numberOfCards[currentCardIndex];
+	}
+}
+
+Console.WriteLine("Day 4A");
+Console.WriteLine($"Sum of scratch card points: {sumOfScratchCardPoints}");
+
+Console.WriteLine("Day 4B");
+Console.WriteLine($"Number of cards: {numberOfCards.Sum()}");
